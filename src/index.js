9c0185ee7336812974810df2ccb6fd2708978774
Cloud8023 <107541981+Cloud8023@users.noreply.github.com>
2024-06-25 21:50:11 +0800

    index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.acgmemory.eu.org": dockerHub,
+  "quay.acgmemory.eu.org": "https://quay.io",
+  "gcr.acgmemory.eu.org": "https://gcr.io",
+  "k8s-gcr.acgmemory.eu.org": "https://k8s.gcr.io",
+  "k8s.acgmemory.eu.org": "https://registry.k8s.io",
+  "ghcr.acgmemory.eu.org": "https://ghcr.io",
+  "cloudsmith.acgmemory.eu.org": "https://docker.cloudsmith.io",
+  "ecr.acgmemory.eu.org": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.acgmemory.eu.org": dockerHub,
 };
 
 function routeByHosts(host) {
